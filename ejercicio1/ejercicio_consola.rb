d4f83a68946f12f42dc007469441c058ce5adf3b
Luis Alberto Osorio Lomeña <osoriolomena@gmail.com>
2021-08-30 22:54:20 -0400

    agregando el primer commit

--- ejercicio1/ejercicio_consola.rb
+++ ejercicio1/ejercicio_consola.rb
@@ -1,1 +1,19 @@
 puts "¡Hola Mundo!"
+puts("como estas")
+
+
+puts "¿Cual es tu nombre?"
+nombre_usuario = gets.chomp
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts "¿Qué edad tienes?"
+edad = gets.chomp
+puts "no representas... #{edad}"
+puts"... representas mucho mas!!!
+puts "¿Donde vives?"
+casa = gets.chomp
+puts "Llueve alguna vez  #{casa} 🤣🤣😂"
+
+
+
