dcf18b2fdde494b054705c760a593792ff5f8919
Luis Alberto Osorio Lomeña <osoriolomena@gmail.com>
2021-08-31 14:32:59 -0400

    trabajo con la ferni

--- ejercicio1/ejercicio_consola.rb
+++ ejercicio1/ejercicio_consola.rb
@@ -2,15 +2,15 @@ puts "¡Hola Mundo!"
 puts("como estas")
 
 
-puts "¿Cual es tu nombre?"
+puts "¿Cual es tu nombre 😎?"
 nombre_usuario = gets.chomp
 puts "hola normal " + nombre_usuario
 puts "hola compuesto #{nombre_usuario}"
 
-puts "¿Qué edad tienes?"
+puts "¿Qué edad tienes 🎂🎂🎉🎉?"
 edad = gets.chomp
 puts "no representas... #{edad}"
-puts"... representas mucho mas!!!"
+puts"... representas mucho mas!!!😜😜😜"
 puts "¿Donde vives?"
 casa = gets.chomp
 puts "Llueve alguna vez  #{casa} 🤣🤣😂"
