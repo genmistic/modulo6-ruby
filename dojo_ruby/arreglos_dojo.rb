96f36837ab104b502b3143eb25be82bdeb40a99f
genmistic <osoriolomena@gmail.com>
2021-09-09 19:47:07 -0300

    oorden

--- dojo_ruby/arreglos_dojo.rb
+++ dojo_ruby/arreglos_dojo.rb
@@ -1,30 +1,41 @@
-# Desafíos
-# Intente los siguientes métodos al menos una vez, 
-# pueden ser en un archivo separado de Ruby o en irb.
+# # Desafíos
+# # Intente los siguientes métodos al menos una vez, 
+# # pueden ser en un archivo separado de Ruby o en irb.
 
-# .at or .fetch
-# .delete
-# .reverse
-# .length
-# .sort
-# .slice
-# .shuffle
-# .join
+a = %w{Pedro Juan Diego Ana Rafaela}
+h = []
+h = { 142, 22, 33, 44 }
+
+# # .at or .fetch
+#.at or .fetch
+
+# puts a(2).fetch
+# h.fetch(:foo)
+
+# # .delete
+# a.delete("pedro")
+
+# # .reverse
+# print "b, al revez es: #{b.reverse}"
+
+# # .length
+# print "el largo de a es: #{a.length}"
+
+# # .sort
+
+# # .slice
+# # .shuffle
+# puts a.shuffle
+
+# # .join
+# print a.join
+
 # .insert
+
 # values_at() -> devuelve un arreglo con los valores específicados en los parametros. 
 # e.g. a = %w{gato perro oso}; puts a.values_at(1,2).join(' and ') #=> "perro y oso"
 
 
-#.at or .fetch
-h = { 142, 22, 33, 44 }
 
-puts a(2).fetch
-h = []
-
-h.fetch(:foo) # no default value, raises error
-# => # ~> -:3:in `fetch': key not found: :foo (KeyError)
-
-h.fetch(:bar, 10) # default value, returns default value
-# => 10
 
 
--- dojo_ruby/range.rb
+++ dojo_ruby/range.rb
@@ -6,9 +6,43 @@
 # .max => devuelve el valor máximo en el Range
 # .min => devuelve el valor mínimo en el Range
 
-a = [1, 2, 3, 4, 5, 5]
- b = [344, 443, 1, 4, 4244 ]
+a = ["pedro", "Luis", "Diego", "Ana", "fernanda", "Rosa"]
+b = [344, 443, 1, 4, 4244 ]
+c = %w{pedro Luis Diego Ana fernanda Rosa} 
 
-if a.include? {b} 
-    print (b)
-end
+
+# if a.include? {b} 
+# print b
+# end
+
+
+# if [a].eql? [b]
+#     puts "Si #{a}"
+#   else
+#       puts "No #{b}"
+# end
+
+# Ejercicio:    print (b)
+
+# print a
+pint a
+# .reverse
+
+# .lengthputs a.
+
+
+
+# .sort
+
+
+# .slice
+# .shuffle
+# puts a.shuffle
+
+# .join
+# puts a.shuffle.join
+
+# .insert
+
+# values_at() -> devuelve un arreglo con los valores específicados en los parametros. 
+# e.g. a = %w{gato perro oso}; puts a.values_at(1,2).join(' and ') #=> "perro y oso"
