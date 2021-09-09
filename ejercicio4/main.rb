dbab60ccb14924e1472e6a5a0812ab2e1218ce94
genmistic <osoriolomena@gmail.com>
2021-09-09 00:06:50 -0300

    modulos en ruby primera clase

---
+++ ejercicio4/main.rb
@@ -0,0 +1,4 @@
+require_relative 'modulo_numeros' #nombre de archivo sin terminacion
+
+include ModuloNumeros
+mostrar_menu
---
+++ ejercicio4/modulo_numeros.rb
@@ -0,0 +1,6 @@
+module ModuloNumeros #comienza con mayuscula y seune comienzo mayuscula
+   
+    def mostrar_menu
+        puts "hola desde el modulo"
+    end
+end
