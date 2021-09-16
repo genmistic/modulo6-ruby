17104576375dfed3240d75013d603ac7788f2f64
genmistic <osoriolomena@gmail.com>
2021-09-15 23:53:49 -0300

    Fin clase guerra monstruos- class - initialize - attr_reader - self

--- ejercicio5/main.rb
+++ ejercicio5/main.rb
@@ -1,25 +1,7 @@
-require_relative 'monstruo'
+require_relative 'batalla'
 
+batalla_nocturna = Batalla.new
+batalla_nocturna.comenzar
 
-
-
-monstruo1 = Monstruos.new()
-monstruo2 = Monstruos.new()
-
-while (monstruo1.vidas > 0 and monstruo2.vidas > 0) do
-monstruo1.combate(monstruo2)
-monstruo2.combate(monstruo1)
-puts
-puts "#{monstruo1.nombre} le quedan #{monstruo1.vidas} vidas" 
-
-puts "#{monstruo2.nombre} le quedan #{monstruo2.vidas} vidas" 
-
-end
-if monstruo1.vidas < monstruo2.vidas
-    puts "GANO #{monstruo2.nombre}"
-else
-    puts "GANO!!!!  #{monstruo1.nombre}"
-end
-
-puts monstruo1
-puts monstruo2
+batalla_nocturna = Batalla.new
+batalla_nocturna.comenzar
--- ejercicio5/monstruo.rb
+++ ejercicio5/monstruo.rb
@@ -1,27 +1,32 @@
 require 'faker'
 
-class Monstruos
+class Monstruo
 
-    attr_accessor :nombre
-    attr_accessor :vidas
-    attr_accessor :ataque
+    attr_reader :nombre     # leer en el atributo
+    attr_accessor :vida     # leer y escribir en el atributo
+    attr_accessor :ataque   # leer y escribir en el atributo
+    
+    # grupo2
+    def initialize(nombre = Faker::Games::StreetFighter.character, vida = rand(100..150), ataque = rand(10..15))
+        @nombre = nombre
+        @vida   = vida
+        @ataque = ataque
+    end
 
-def initialize (nombre = Faker::Games::Pokemon.name, vidas = rand(100..100), ataque = rand(10..15))
-    @nombre = nombre
-    @vidas = vidas
-    @ataque = ataque
+    def atacar(monstruo_enemigo)
+        if rand(1..100) < 70
+            monstruo_enemigo.vida -= @ataque
+            puts "#{@nombre} ataca a #{monstruo_enemigo.nombre} con #{@ataque}"
+            puts "#{monstruo_enemigo.nombre} quedó con: #{monstruo_enemigo.vida} HP"
+            puts
+        else
+            puts "¡#{@nombre} FALLÓ!"
+        end
+    end
+
+    def to_s
+        "#{@nombre} | #{@vida} | #{@ataque}"
+    end
+
+    
 end
-
-def to_s
-    "#{@nombre} | #{@vida} | #{@ataque}"
-end
-
-
-def combate (enemigo)
-  enemigo.vidas -= @ataque
-     
- end
- 
-end
-
-
